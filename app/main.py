21a22d8e47c758f3ca52c086e24469b0933fef8c
Prakhar Bansal <prakharbansal0910@gmail.com>
2025-08-18 08:13:18 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -8,8 +8,16 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept() # wait for client
-    connection.sendall(b"+PONG\r\n")
+    connection, _ = server_socket.accept() # wait for client  
+    try:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(b"+PONG\r\n")
+    finally:
+        connection.close()
+        
 
 
 if __name__ == "__main__":
