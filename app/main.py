dcf3f057dd7f6895e13480bbbf2204b5509db309
Prakhar Bansal <prakharbansal0910@gmail.com>
2025-08-19 23:29:13 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -2,14 +2,11 @@ import socket  # noqa: F401
 from _thread import start_new_thread
 import threading
 
-lock = threading.Lock()
-
 def handle_client(connection):
     try:
         while True:
             data = connection.recv(1024)
-            if not data:
-                #lock.release()     
+            if not data:    
                 break
             connection.sendall(b"+PONG\r\n")
     except Exception as e:
@@ -18,21 +15,18 @@ def handle_client(connection):
         connection.close()
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     try:
         while True:
             connection, _ = server_socket.accept() # wait for client  
-            #lock.acquire()
-            start_new_thread(handle_client,(connection,))
+            start_new_thread(handle_client,(connection,)) # Handle concurrent clients
     except Exception as e:
         print(f"Server error: {e}")
     finally:
         server_socket.close()
+    '''
     try: # respond to multiple PINGs
         while True:
             data = connection.recv(1024)
@@ -41,6 +35,7 @@ def main():
             connection.sendall(b"+PONG\r\n")
     finally:
         connection.close()
+    '''
 
 
 
