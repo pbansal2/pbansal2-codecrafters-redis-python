b43e94a2c6acc73ac83d9fb2df97476c01f6c68e
Prakhar Bansal <prakharbansal0910@gmail.com>
2025-08-19 22:01:13 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -20,10 +20,13 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    while True:
-        connection, _ = server_socket.accept() # wait for client  
-        lock.acquire()
-        start_new_thread(handle_client,(connection,))
+    try:
+        while True:
+            connection, _ = server_socket.accept() # wait for client  
+            lock.acquire()
+            start_new_thread(handle_client,(connection,))
+    finally:
+        server_socket.close()
     try: # respond to multiple PINGs
         while True:
             data = connection.recv(1024)
