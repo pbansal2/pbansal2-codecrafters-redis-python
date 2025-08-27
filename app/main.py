080c29dae7fbb1b5debaaa21893ebac1ec59e1be
Prakhar Bansal <prakharbansal0910@gmail.com>
2025-08-27 12:03:05 +0200

    [any message]

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def handle_client(connection):
                 key = parts[3]
                 if key in store:
                     value = store[key]
-                    print(f"value")
+                    print(f"{value}")
                     response = f"${len(value)}\r\n{value}\r\n"
                 else:
                     response = "$-1\r\n"
